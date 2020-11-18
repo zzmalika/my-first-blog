11a7c149f1e98ccc8931ae9ebf3ae317bc3e008a
zzmalika <malika.zhusupbaeva@gmail.com>
2020-11-18 16:52:06 +0600

    extensions

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+from django.urls import path, include
+from django.contrib import admin
+from django.contrib.auth import views as view_django
 from . import views
 
 urlpatterns = [
@@ -7,4 +10,9 @@ urlpatterns = [
     path('post/new/', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
     path('delete/<int:pk>', views.post_delete, name='post_delete'),
+    path('drafts/', views.post_draft_list, name='post_draft_list'),
+    path('post/<pk>/publish/', views.post_publish, name='post_publish'),
+    path('accounts/login/', view_django.LoginView.as_view(), name='login'),
+    path('accounts/logout/', view_django.LogoutView.as_view(next_page='/'), name='logout'),
+
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -3,6 +3,7 @@ from .models import Post
 from django.utils import timezone
 from .forms import PostForm
 from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
@@ -15,21 +16,20 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
-
+@login_required
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
 
-
+@login_required
 def post_edit(request, pk):
     post = get_object_or_404(Post, pk=pk)
     if request.method == "POST":
@@ -37,15 +37,25 @@ def post_edit(request, pk):
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
-            post.published_date = timezone.now()
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
 
-
+@login_required
 def post_delete(request, pk):
-    post_to_delete = Post.objects.get(id=pk)
-    post_to_delete.delete()
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
     return redirect('post_list')
+
+@login_required
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull=True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'posts': posts})
+
+@login_required
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
